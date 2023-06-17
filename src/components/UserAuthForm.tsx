ca3977219febb59d4f951d6a25a2ab3d81589038
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-17 20:52:59 +0600

    added functional toaster

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -5,17 +5,24 @@ import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 import { signIn } from "next-auth/react";
 import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
+  const { toast } = useToast();
   const loginWithGoogle = async () => {
     setIsLoading(true);
     try {
+      throw new Error();
       await signIn("google");
     } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
