ac1cbc9d16bc347894f54c0b4eb6ef2c4c7650c4
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-19 12:05:32 +0600

    finished the dropdown

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,6 +1,6 @@
 import { User } from "next-auth";
 import { FC } from "react";
-import { Avatar, AvatarFallback } from "./ui/avatar";
+import { Avatar, AvatarFallback } from "./ui/Avatar";
 import Image from "next/image";
 import { Icons } from "./Icons";
 import { AvatarProps } from "@radix-ui/react-avatar";
