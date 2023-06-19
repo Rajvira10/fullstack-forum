7b41d66d1862c453a0c28fa0bc3e70c35aa62e5b
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-19 12:23:00 +0600

    added a route intercept for the signin modal

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -15,7 +16,7 @@ const page: FC = ({}) => {
             "self-start -mt-20"
           )}
         >
-          Home
+          <ChevronLeft className="mr-2 h-4 w-4" /> Home
         </Link>
 
         <SignIn />
