fcf07e6518ef2811b10d4b1c06820260213ac3f3
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-19 12:09:30 +0600

    fixed the Navbar showing error because of async in tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error server component */}
         <Navbar />
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
