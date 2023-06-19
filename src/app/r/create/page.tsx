2af425ce7269c15b77793b602804b0c6b6dc6c6e
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-20 00:47:59 +0600

    created the r/create page with the input field from shan

---
+++ src/app/r/create/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Input } from "@/components/ui/Input";
+import { useState } from "react";
+
+const Page = () => {
+  const [input, setInput] = useState<string>("");
+  return (
+    <div className="container flex items-center h-full max-w-3xl mx-auto">
+      <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-semibold">Create a community</h1>
+        </div>
+        <hr className="bg-zinc-500 h-px" />
+        <div>
+          <p className="text-lg font-medium">Name</p>
+          <p className="text-xs pb-2">
+            Community names including capitalization cannot be changed.
+          </p>
+          <div className="relative">
+            <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items">
+              r/
+            </p>
+            <Input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="pl-6"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
