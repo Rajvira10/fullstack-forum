ab16b7ecccedc87e7aa5c0ffff4f7d0ff54caca0
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-21 16:07:41 +0600

    added the creating community functionalities

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,10 +1,51 @@
 "use client";
 
+import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { CreateSubredditPayload } from "@/lib/validators/subreddit";
+import { toast } from "@/hooks/use-toast";
 
 const Page = () => {
+  const router = useRouter();
   const [input, setInput] = useState<string>("");
+
+  const { mutate: createCommunity, isLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: CreateSubredditPayload = {
+        name: input,
+      };
+      const { data } = await axios.post("/api/subreddit", payload);
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Community already exists",
+            description: "Please choose a different subreddit name",
+            variant: "destructive",
+          });
+        }
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invalid subreddit name",
+            description: "Please choose a name between 3 and 21 characters",
+            variant: "destructive",
+          });
+        }
+        if (err.response?.status === 401) {
+          return toast({
+            title: "You must be signed in to create a community",
+            variant: "destructive",
+          });
+        }
+      }
+    },
+  });
   return (
     <div className="container flex items-center h-full max-w-3xl mx-auto">
       <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
@@ -27,6 +68,18 @@ const Page = () => {
               className="pl-6"
             />
           </div>
+        </div>
+        <div className="flex justify-end gap-4">
+          <Button variant="subtle" onClick={() => router.back()}>
+            Cancel
+          </Button>
+          <Button
+            isLoading={isLoading}
+            disabled={input.length === 0}
+            onClick={() => createCommunity()}
+          >
+            Create Community
+          </Button>
         </div>
       </div>
     </div>
