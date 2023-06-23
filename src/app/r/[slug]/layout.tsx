aa2f23736df44d2bc1fcf2892b2ce50eb6ee0617
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-23 19:21:18 +0600

    defining types of the cached data of redis

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -61,7 +61,7 @@ const Layout = async ({
       <div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6"> {children}</div>
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+          <div className="md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4">
               <p className="font-semibold py-3">About r/{subreddit.name}</p>
             </div>
---
+++ src/types/redist.d.ts
@@ -0,0 +1,10 @@
+import { VoteType } from "@prisma/client";
+
+export type CachedPost = {
+  id: string;
+  title: string;
+  content: string;
+  authorUsername: string;
+  currentVote: VoteType | null;
+  createdAt: Date;
+};
