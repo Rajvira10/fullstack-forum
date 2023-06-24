8b742cb6bd0e699ffe7ba2d52408a3ba2ed19c57
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-24 16:54:10 +0600

    added the comment vote functionality

---
+++ src/components/CommentVotes.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { usePrevious } from "@mantine/hooks";
+import { CommentVote, VoteType } from "@prisma/client";
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { CommentVoteRequest } from "@/lib/validators/vote";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
+
+interface CommentVoteProps {
+  commentId: string;
+  initialVotesAmt: number;
+  initialVote?: Pick<CommentVote, "type">;
+}
+
+const CommentVote: FC<CommentVoteProps> = ({
+  commentId,
+  initialVotesAmt,
+  initialVote,
+}) => {
+  const { loginToast } = useCustomToast();
+  const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
+  const [currentVote, setCurrentVote] = useState(initialVote);
+  const prevVote = usePrevious(currentVote);
+
+  const { mutate: vote, isLoading: isVoting } = useMutation({
+    mutationFn: async (voteType: VoteType) => {
+      const payload: CommentVoteRequest = { commentId, voteType };
+      await axios.patch("/api/subreddit/post/comment/vote", payload);
+    },
+    onError: (err, voteType) => {
+      if (voteType === "UP") {
+        setVotesAmt((prev) => prev - 1);
+      } else {
+        setVotesAmt((prev) => prev + 1);
+      }
+
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+      return toast({
+        title: "Something went wrong",
+        description: "Your vote was not registered, please try again.",
+        variant: "destructive",
+      });
+    },
+    onMutate: (type) => {
+      if (currentVote?.type === type) {
+        setCurrentVote(undefined);
+        if (type === "UP") {
+          setVotesAmt((prev) => prev - 1);
+        }
+        if (type === "DOWN") {
+          setVotesAmt((prev) => prev + 1);
+        }
+      } else {
+        setCurrentVote({ type });
+        if (type === "UP") {
+          setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+        } else if (type === "DOWN") {
+          setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+        }
+      }
+    },
+  });
+
+  return (
+    <div className="flex gap-1">
+      <Button
+        isLoading={isVoting}
+        onClick={() => vote("UP")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
+        <ArrowBigUp
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-emerald-500 fill-emerald-500": currentVote?.type === "UP",
+          })}
+        />
+      </Button>
+      <p className="text-center py-2 font-medium text-sm text-zinc-900">
+        {votesAmt}
+      </p>
+
+      <Button
+        isLoading={isVoting}
+        onClick={() => vote("DOWN")}
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+      >
+        <ArrowBigDown
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-red-500 fill-red-500": currentVote?.type === "DOWN",
+          })}
+        />
+      </Button>
+    </div>
+  );
+};
+
+export default CommentVote;
--- src/components/CommentsSections.tsx
+++ src/components/CommentsSections.tsx
@@ -26,6 +26,9 @@ const CommentsSections = async ({ postId }: CommentsSectionsProps) => {
         },
       },
     },
+    orderBy: {
+      createdAt: "desc",
+    },
   });
   return (
     <div className="flex flex-col gap-y-4 mt-4">
@@ -51,7 +54,40 @@ const CommentsSections = async ({ postId }: CommentsSectionsProps) => {
             return (
               <div key={topLevelComment.id} className="flex flex-col">
                 <div className="mb-2">
-                  <PostComment comment={topLevelComment} />
+                  <PostComment
+                    postId={postId}
+                    currentVote={topLevelCommentVote}
+                    votesAmt={topLevelCommentVotesAmt}
+                    comment={topLevelComment}
+                  />
+                  {topLevelComment.replies
+                    .sort((a, b) => b.votes.length - a.votes.length)
+                    .map((reply) => {
+                      const replyVotesAmt = topLevelComment.votes.reduce(
+                        (acc, vote) => {
+                          if (vote.type === "UP") return acc + 1;
+                          if (vote.type === "DOWN") return acc - 1;
+                          return acc;
+                        },
+                        0
+                      );
+                      const replyVote = topLevelComment.votes.find(
+                        (vote) => vote.userId === session?.user.id
+                      );
+                      return (
+                        <div
+                          key={reply.id}
+                          className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                        >
+                          <PostComment
+                            comment={reply}
+                            currentVote={replyVote}
+                            votesAmt={replyVotesAmt}
+                            postId={postId}
+                          />
+                        </div>
+                      );
+                    })}
                 </div>
               </div>
             );
