591c6890caad5c4b3ba84bd17fc0e4b30e19288f
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-24 19:20:44 +0600

    some last tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default async function Home() {
   const session = await getAuthSession();
