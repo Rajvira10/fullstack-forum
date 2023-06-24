b6b973c1a05381c2272a761bc40624db99515b7d
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-24 16:59:35 +0600

    added the comment vote functionality

--- src/components/CommentsSections.tsx
+++ src/components/CommentsSections.tsx
@@ -60,35 +60,32 @@ const CommentsSections = async ({ postId }: CommentsSectionsProps) => {
                     votesAmt={topLevelCommentVotesAmt}
                     comment={topLevelComment}
                   />
-                  {topLevelComment.replies
-                    .sort((a, b) => b.votes.length - a.votes.length)
-                    .map((reply) => {
-                      const replyVotesAmt = topLevelComment.votes.reduce(
-                        (acc, vote) => {
-                          if (vote.type === "UP") return acc + 1;
-                          if (vote.type === "DOWN") return acc - 1;
-                          return acc;
-                        },
-                        0
-                      );
-                      const replyVote = topLevelComment.votes.find(
-                        (vote) => vote.userId === session?.user.id
-                      );
-                      return (
-                        <div
-                          key={reply.id}
-                          className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
-                        >
-                          <PostComment
-                            comment={reply}
-                            currentVote={replyVote}
-                            votesAmt={replyVotesAmt}
-                            postId={postId}
-                          />
-                        </div>
-                      );
-                    })}
                 </div>
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length)
+                  .map((reply) => {
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === "UP") return acc + 1;
+                      if (vote.type === "DOWN") return acc - 1;
+                      return acc;
+                    }, 0);
+                    const replyVote = reply.votes.find(
+                      (vote) => vote.userId === session?.user.id
+                    );
+                    return (
+                      <div
+                        key={reply.id}
+                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    );
+                  })}
               </div>
             );
           })}
