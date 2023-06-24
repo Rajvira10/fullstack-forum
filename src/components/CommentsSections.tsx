b9bc22b00dd691315c049f098f31bca6b0c64328
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-24 15:41:12 +0600

    comment section done

--- src/components/CommentsSections.tsx
+++ src/components/CommentsSections.tsx
@@ -2,6 +2,7 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { FC } from "react";
 import PostComment from "./PostComment";
+import CreateComment from "./CreateComment";
 
 interface CommentsSectionsProps {
   postId: string;
@@ -29,7 +30,7 @@ const CommentsSections = async ({ postId }: CommentsSectionsProps) => {
   return (
     <div className="flex flex-col gap-y-4 mt-4">
       <hr className="w-full h-px my-6" />
-      <CreateComment />
+      <CreateComment postId={postId} />
 
       <div className="flex flex-col gap-y-6 mt-4">
         {comments
--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -1,11 +1,78 @@
-import { FC } from "react";
+"use client";
 
-interface CreateCommentProps {}
+import { FC, useState } from "react";
+import { Label } from "./ui/Label";
+import { Textarea } from "./ui/Textarea";
+import { Button } from "./ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import { CommentRequest } from "@/lib/validators/comment";
+import axios, { AxiosError } from "axios";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
-const CreateComment: FC<CreateCommentProps> = ({}) => {
+interface CreateCommentProps {
+  postId: string;
+  replyToId?: string;
+}
+
+const CreateComment: FC<CreateCommentProps> = ({ postId, replyToId }) => {
+  const { loginToast } = useCustomToast();
+  const router = useRouter();
+  const [input, setInput] = useState<string>("");
+
+  const { mutate: comment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+
+      const { data } = await axios.patch(
+        "/api/subreddit/post/comment",
+        payload
+      );
+      return data;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong. Please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: (data) => {
+      router.refresh();
+      setInput("");
+    },
+  });
   return (
     <div className="grid w-full gap-1.5">
-      <Label />
+      <Label htmlFor="comment">Your comment</Label>
+      <div className="mt-2">
+        <Textarea
+          id="comment"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          rows={3}
+          placeholder="What are your thoughts?"
+        />
+        <div className="mt-2 flex justify-end">
+          <Button
+            isLoading={isLoading}
+            disabled={input.length === 0}
+            onClick={() => comment({ postId, text: input, replyToId })}
+          >
+            Post
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
