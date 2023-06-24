8283fff899113f46a6dbd60a4e18aa956d6f172c
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-24 18:58:28 +0600

    fixed some spelling issues

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -9,12 +9,12 @@ const SignIn = () => {
         <Icons.logo className="mx-auto h-6 w-6" />
         <h1 className="text-2xl font-semibold tracking-light">Welcome back</h1>
         <p className="text-sm max-w-xs mx-auto">
-          By continuing, you are setting up a Breadit account and agree to our
+          By continuing, you are setting up a StudyHUB account and agree to our
           User Agreement and Privacy Policy
         </p>
         <UserAuthForm />
         <p className="px-8 text-center text-sm text-zinc-700 ">
-          New to Bredit?{" "}
+          New to StudyHUB?{" "}
           <Link
             href="/sign-up"
             className="hover:text-zinc-800 text-sm underline underline-offset-4"
